51c509953c74ed570dc8258866a7ce12c7b574a2
Vít Šalomon <vit@salomon.cloud>
2025-03-12 14:52:23 +0100

    fix: added typecheck to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-	'*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
+	'*.{ts,tsx}': ['eslint --fix', 'prettier --write', () => 'tsc --noEmit'],
 };
